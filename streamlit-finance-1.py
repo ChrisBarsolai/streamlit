523da1de3d28abcdbb88d42dde9737d724e4c464
chrisbarsolai <chrisbarsolai@gmail.com>
2021-04-30 14:56:03 +0300

    First Web App

---
+++ streamlit-finance-1.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+    # Simple Stock Price App
+
+    Shown are the stock closing price and volume of Google!
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+# Define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for the ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
